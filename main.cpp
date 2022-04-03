419b5b27d44bf36469220fd642d9ea5ded8ec42b
Lukka14 <102803654+Lukka14@users.noreply.github.com>
2022-04-03 17:01:36 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,63 +1,113 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
 struct samkutxedi{
     double a;
     double b;
     double c;
-    double angle (int n=0){
-        if(!isTriangle())
-            return -1;
-        switch (n){
-            case 0:
-                return acos((b*b+c*c-a*a)/(2*b*c) );
-            case 1:
-                return acos((b*b+a*a-c*c)/(2*b*a));
-            default:
-                return acos((a*a+c*c-b*b)/(2*a*c));
-        }
-    }
-    void input(){
-        cout<<"Choose known triangle sides or angles: \n";
-        cout<<"1) 2 sides and 1 angle.\n";
-        cout<<"2) 1 side and 2 angles.\n";
-        cout<<"3) All 3 triangle sides.\n";
-        cout<<"Enter 1, 2 or 3: ";
-        int n; cin>>n;
-        switch (n) {
-            case 1:
-                break;
-            case 2:
-                cout << "Enter the side size: ";
 
-        }
+    double Alpha;
+    double Beta;
+    double Gamma;
+    void oriGverdi(){
+        double kutxe;
+        cout<<"შეიყვანეთ a და b გვერდები: ";
+        cin>>a>>b;
+        cout << "შეიყვანეთ კუთხის ზომა რადიანებში: ";
+        cin >> kutxe;
+        string mopirdapireGverdi = "a";
+        cout << "რომელია კუთხის მოპირდაპირე გვერდი? ";
         do{
+            cout << "შეიყვანეთ a, b ან c: ";
+            cin >> mopirdapireGverdi;
+        }while(mopirdapireGverdi != "a" && mopirdapireGverdi != "b" && mopirdapireGverdi != "c");
+        if (mopirdapireGverdi == "a") {
+             Alpha=kutxe;
+             Beta = asin((b * sin(Alpha)) / a);
+             Gamma = M_PI - Alpha - Beta;
+            c = (a * sin(Gamma) / sin(Alpha));
+        }
+        if(mopirdapireGverdi == "b"){
+             Beta=kutxe;
+             Alpha = asin((a * sin(Beta)) / b);
+             Gamma = M_PI - Beta - Alpha;
+             c = (b * sin(Gamma) / sin(Beta));
+        }
+        if(mopirdapireGverdi == "c") {
+            Gamma=kutxe;
+            c = sqrt(a * a + b * b - 2 * a * b * cos(kutxe));
+            Alpha = asin((a* sin(Gamma))/c);
+            Beta = M_PI - Alpha - Gamma;
+        }
+        isTriangle();
+    }
+
+    void oriKutxe(){
+        int n;
+        double gverdi;
+        cout << "შეიყვანეთ გვერდის სიგრძე: ";
+        cin>>gverdi;
+        cout<<"შეიყვანეთ Alpha და Beta კუთხეები: ";
+        cin>>Alpha>>Beta;
+        cout<<"რომელია ცნობილი გვერდის პირდაპირ მდებარე კუთხე?\n";
+        do{
+            cout<<"1) Alpha\n2) Beta\n3) Gamma\n";
+            cout<<"აირჩიეთ შესაბამისი ციფრი: ";
+            cin>>n;
+        }while(n!=1 && n!=2 && n!=3);
+        if(n==1){
+            a=gverdi;
+            Gamma=M_PI-Alpha-Beta;
+            c = (a * sin(Gamma) / sin(Alpha));
+            b = (a*sin(Beta))/ sin(Alpha);
+        }
+        if(n==2){
+            b=gverdi;
+            Gamma=M_PI-Alpha-Beta;
+            a = (sin(Alpha)*b)/ sin(Beta);
+            c = (b * sin(Gamma) / sin(Beta));
+        }
+        if(n==3){
+            c=gverdi;
+            Gamma=M_PI-Alpha-Beta;
+            a=(sin(Alpha)*c)/sin(Gamma);
+            b=(sin(Beta)*c)/sin(Gamma);
+        }
+        isTriangle();
+    }
+
+    void samiGverdi(){
+        cout<<"შეიყვანეთ სამკუთხედის გვერდები: \n";
             cout<<"a="; cin>>a; a=fabs(a);
             cout<<"b="; cin>>b; b=fabs(b);
             cout<<"c="; cin>>c; c=fabs(c);
-        }while(!isTriangle());
+            isTriangle();
+            Alpha= acos((b*b+c*c-a*a)/(2*b*c));
+            Beta= acos((a*a+c*c-b*b)/(2*a*c));
+            Gamma= acos((b*b+a*a-c*c)/(2*a*b));
     }
-    void oriGverdi(){
-        double Alpha;
-        cout<<"Enter a and b sides: ";
-        cin>>a>>b;
-        cout << "Enter the angle size in radians: ";
-        cin >> Alpha;
-        char oppositeside = 'a';
-        cout << "Which is the opposite side of the angle ";
-        do {
-            cout << "type a, b or c: ";
-            cin >> oppositeside;
-        }while(oppositeside!='a' || oppositeside!='b' || oppositeside!='c');
-        if (oppositeside == 'a') {
-            double Beta = asin((b * sin(Alpha)) / a);
-            double Gamma = M_PI - Alpha - Beta;
-            c = (b * sin(Gamma) / sin(Alpha));
-        }
-        if(oppositeside== 'b'){
 
+    void sheyvana(){
+        cout<<"აირჩიეთ რა არის ცნობილი:\n";
+        cout<<"1) 2 გვერდი და 1 კუთხე.\n";
+        cout<<"2) 1 გვერდი და 2 კუთხე.\n";
+        cout<<"3) სამკუთხედის სამივე გვერდი.\n";
+        int n;
+        do {
+            cout << "შეიყვანეთ 1, 2 ან 3: ";
+            cin >> n;
+        }while(n!=1 && n!=2 && n!=3);
+        switch (n) {
+            case 1:
+                oriGverdi();
+                break;
+            case 2:
+                oriKutxe();
+                break;
+            case 3:
+                samiGverdi();
+                break;
         }
     }
     double P(){
@@ -66,50 +116,88 @@ struct samkutxedi{
 
     double S(){
         double p=P();
-        if(isTriangle()) return sqrt(p * (p - a) * (p - b) * (p - c));
-        else return -1;
+        return sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
-    bool isTriangle(){
+    double height(double tmp){
+        return (2*S())/tmp;
+    }
+
+    double chaxazulisS(){
+        double r=S()/P();
+        return M_PI*r*r;
+    }
+
+    double shemoxazulisS(){
+        double R=a/(2* sin(Alpha));
+        return M_PI*R*R;
+    }
+
+
+
+    void isTriangle(){
         double p=P();
-        if (sqrt(p*(p-a)*(p-b)*(p-c))>0)
-            return true;
-        else
-            return false;
-    }
-
-    double mediana(int n=0){
-        if(!isTriangle())  return -1;
-        switch(n){
-            case 0:
-                return sqrt(2*(b*b+c*c)-a*a)/2;
-            case 1:
-                return sqrt(2*(a*a+c*c)-b*b)/2;
-            default:
-                return sqrt(2*(a*a+b*b)-c*c)/2;
+        if (sqrt(p*(p-a)*(p-b)*(p-c))<=0){
+            cout<<"შეყვანილი ინფორმაცია არასწორია, ასეთი სამკუთხედი არ არსებობს. :(";
+            exit(-1);
         }
     }
 
-    void display(bool onestrng){
+    double mediana(int n=0){
+        switch(n){
+            case 0:
+                return sqrt(2*(b*b+c*c)-a*a)/2.0;
+            case 1:
+                return sqrt(2*(a*a+c*c)-b*b)/2.0;
+            default:
+                return sqrt(2*(a*a+b*b)-c*c)/2.0;
+        }
+    }
+
+    double biseqtrisa(string gverdi="a"){
+        if (gverdi == "a") {
+            return (2*sqrt(b*c*P()*(P()-a)))/(b+c);
+        }
+        if(gverdi == "b"){
+            return (2*sqrt(a*c*P()*(P()-b)))/(a+c);
+        }
+        if(gverdi == "c") {
+            return (2*sqrt(b*a*P()*(P()-c)))/(b+a);;
+        }
+    }
+
+    double damrgvaleba(double tmp=0,int x=10){
+        return round(tmp*pow(10,x))/(pow(10,x)*1.0);
+    }
+
+    void gamotana(bool onestrng){
         char sep;
         if(!onestrng)
             sep='\n';
         else	sep='\t';
+        cout<<"a="<<damrgvaleba(a)<<sep
+            <<"b="<<damrgvaleba(b)<<sep
+            <<"c="<<damrgvaleba(c)<<sep
+            <<"α="<<damrgvaleba(Alpha)<<sep
+            <<"ß="<<damrgvaleba(Beta)<<sep
+            <<"ɣ="<<damrgvaleba(Gamma)<<sep
+            <<"m(a)="<<damrgvaleba(mediana(0))<<sep
+            <<"m(b)="<<damrgvaleba(mediana(1))<<sep
+            <<"m(c)="<<damrgvaleba(mediana(2))<<sep
+            <<"h(a)="<<damrgvaleba(height(a))<<sep
+            <<"h(b)="<<damrgvaleba(height(b))<<sep
+            <<"h(c)="<<damrgvaleba(height(c))<<sep
+            <<"B(a)="<<damrgvaleba(biseqtrisa("a"))<<sep
+            <<"B(b)="<<damrgvaleba(biseqtrisa("b"))<<sep
+            <<"B(c)="<<damrgvaleba(biseqtrisa("c"))<<sep
+            <<"P="<<damrgvaleba(2*P())<<sep
+            <<"S="<<damrgvaleba(S())<<sep
+            <<"area="<<damrgvaleba(chaxazulisS())<<sep
+            <<"AREA="<<damrgvaleba(shemoxazulisS())<<sep;
 
-        cout<<a<<sep
-            <<b<<sep
-            <<c<<sep
-            <<"α = "<<angle(0)<<sep
-            <<"ß = "<<angle(1)<<sep
-            <<"ɣ = "<<angle(2)<<sep
-            <<mediana(0)<<sep
-            <<mediana(1)<<sep
-            <<mediana(2)<<sep
-            <<"P = "<<P()<<sep
-            <<"S = "<<S()<<endl;
     }}A;
 
 int main(){
-    A.input();
-    A.display(true);
+    A.sheyvana();
+    A.gamotana(true);
 }
